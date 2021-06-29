a1a135a0263008cb3c7413a535fa056798c41d64
JacobUkokobili <ukokobili.jacob@gmail.com>
2021-06-29 19:32:06 +0100

    adding third commit

--- 03_data_analysis.sql
+++ 03_data_analysis.sql
@@ -4,10 +4,41 @@ insights on how the casual riders and members use Cyclistic rideshare differentl
 
 -- Calculating the total number of rides
 SELECT 
-	COUNT(ride_id)
+	COUNT(ride_id) AS total_number_rides
 FROM 
 	"202004_202103_tripdata"
 -- Total number of rides is 3.4 million
+
+
+-- Calculating the number of rideable bikes  by customer
+SELECT
+	customer,
+	rideable_type,
+	COUNT(rideable_type) AS total_number_rides
+FROM 
+	"202004_202103_tripdata"
+GROUP BY 1,2
+ORDER BY COUNT(rideable_type) DESC
+-- Docked bikes are preferred by Casual riders and Annual members 
+-- although most classic bikes and electric bikes also popular.
+
+
+-- Calculating the total number of duration
+SELECT 
+	ROUND(SUM(duration_in_secs)) AS total_number_secs
+FROM 
+	"202004_202103_tripdata"
+-- Total number of duration is 5720854937 secs
+
+
+-- Calculating the total number of duration by casual riders and annual members
+SELECT 
+	customer,
+	ROUND(SUM(duration_in_secs)) AS number_of_customer,
+FROM 
+	"202004_202103_tripdata"
+GROUP BY 1
+-- Casual riders cover longer rider lenght overtime compared to annual members
 
 
 -- Calculating the average ride duration
@@ -36,8 +67,8 @@ SELECT
 FROM 
 	"202004_202103_tripdata"
 GROUP BY 1
--- Casual riders cover more duration on the average than members although members
--- tend to have the highest ride duration.
+-- Casual riders cover more duration on the average than annual members 
+-- although members tend to have the highest ride duration.
 
 
 -- Calculating the average duration of users per day of week
@@ -62,8 +93,8 @@ AS pivot_table (
 	Fri double precision,
 	Sat double precision
 );
--- There are longer rides on Weds for both customer types compared to other days of the week
--- and from observation, casual riders on weekends compared to memebers.
+-- The average duration of rides by casual riders is more than 
+-- twice that of annuals over any given day of the week cumulatively.
 
 
 -- Calculating the number of rides for users by day of week
@@ -97,7 +128,7 @@ SELECT
 FROM 
 	"202004_202103_tripdata"
 GROUP  BY 1	
--- hmmmm, we actually have more members as customers 
+-- hmmmm, we actually have more annual members as customers 
 
 
 -- Calculating the number of rides for users by year
@@ -117,8 +148,8 @@ AS pivot_table (
     "2020" bigint, 
     "2021" bigint
 );
--- There were more members compared to casual for 
--- both 2020 qnd 2021 respectively.
+-- There were more annual members compared to casual riders for 
+-- both 2020 and 2021 respectively.
 
 
 -- Calculating the number ride and average duration by customer, rideable type and day of the week
